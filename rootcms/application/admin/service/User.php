7d9e2b016f8ad8d042663c5469f7778b4d4a975d
wb1491 <wb1491@126.com>
2016-08-25 11:55:35 +0800

    增加后台admin模块的后台用户认证服务

---
+++ rootcms/application/admin/service/User.php
@@ -0,0 +1,158 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | rootCMS 后台用户服务
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015-2016 http://www.rootcms.cn, All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 
+// +----------------------------------------------------------------------
+
+namespace app\admin\service;
+
+class User {
+
+    //存储用户uid的Key
+    const userUidKey = 'CMS_admin_userid';
+    //超级管理员角色id
+    const administratorRoleId = 1;
+
+    //当前登录会员详细信息
+    private static $userInfo = array();
+
+    /**
+     * 连接后台用户服务
+     * @staticvar \app\admin\service\Cache $systemHandier
+     * @return \app\admin\service\Cache
+     */
+    static public function getInstance() {
+        static $handier = NULL;
+        if (empty($handier)) {
+            $handier = new User();
+        }
+        return $handier;
+    }
+
+    /**
+     * 魔术方法
+     * @param type $name
+     * @return null
+     */
+    public function __get($name) {
+        //从缓存中获取
+        if (isset(self::$userInfo[$name])) {
+            return self::$userInfo[$name];
+        } else {
+            $userInfo = $this->getInfo();
+            if (!empty($userInfo)) {
+                return $userInfo[$name];
+            }
+            return NULL;
+        }
+    }
+
+    /**
+     * 获取当前登录用户资料
+     * @return array 
+     */
+    public function getInfo() {
+        if (empty(self::$userInfo)) {
+            self::$userInfo = $this->getUserInfo($this->isLogin());
+        }
+        return !empty(self::$userInfo) ? self::$userInfo : false;
+    }
+
+    /**
+     * 检验用户是否已经登陆
+     * @return boolean 失败返回false，成功返回当前登陆用户基本信息
+     */
+    public function isLogin() {
+        $userId = \libs\util\Encrypt::authcode(session(self::userUidKey), 'DECODE');
+        if (empty($userId)) {
+            return false;
+        }
+        return (int) $userId;
+    }
+
+    //登录后台
+    public function login($identifier, $password) {
+        if (empty($identifier) || empty($password)) {
+            return false;
+        }
+        //验证
+        $userInfo = $this->getUserInfo($identifier, $password);
+        if (false == $userInfo) {
+            //记录登录日志
+            $this->record($identifier, $password, 0);
+            return false;
+        }
+        //记录登录日志
+        $this->record($identifier, $password, 1);
+        //注册登录状态
+        $this->registerLogin($userInfo->toArray());
+        return true;
+    }
+
+    /**
+     * 检查当前用户是否超级管理员
+     * @return boolean
+     */
+    public function isAdministrator() {
+        $userInfo = $this->getInfo();
+        if (!empty($userInfo) && $userInfo['role_id'] == self::administratorRoleId) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 注销登录状态
+     * @return boolean
+     */
+    public function logout() {
+        session(NULL);
+        return true;
+    }
+
+    /**
+     * 记录登陆日志
+     * @param type $identifier 登陆方式，uid,username
+     * @param type $password 密码
+     * @param type $status
+     */
+    private function record($identifier, $password, $status = 0) {
+        //登录日志
+         model('admin/Loginlog')->addLoginLogs(array(
+            "username" => $identifier,
+            "status" => $status,
+            "password" => $status ? '密码保密' : $password,
+            "info" => is_int($identifier) ? '用户ID登录' : '用户名登录',
+        ));
+    }
+
+    /**
+     * 注册用户登录状态
+     * @param array $userInfo 用户信息
+     */
+    private function registerLogin(array $userInfo) {
+        //写入session
+        session(self::userUidKey, \libs\util\Encrypt::authcode((int) $userInfo['id'], ''));
+        //更新状态
+         model('admin/User')->loginStatus((int) $userInfo['id']);
+        //注册权限
+        \libs\system\RBAC::saveAccessList((int) $userInfo['id']);
+    }
+
+    /**
+     * 获取用户信息
+     * @param type $identifier 用户名或者用户ID
+     * @return boolean|array
+     */
+    private function getUserInfo($identifier, $password = NULL) {
+        if (empty($identifier)) {
+            return false;
+        }
+        return  model('admin/User')->getUserInfo($identifier, $password);
+    }
+
+}
