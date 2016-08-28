42928837263fc2c500965e520487b6eba8cf757d
wb1491@126.com <wb1491@126.com>
2016-08-28 21:20:20 +0800

    修正内容关联模型的构造函数在查询返回时无法正常构构建对象问题！

--- rootcms/application/content/model/ContentData.php
+++ rootcms/application/content/model/ContentData.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// +----------------------------------------------------------------------
+// | rootCMS 内容模型
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015-2016 http://www.rootcms.cn, All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 
+// +----------------------------------------------------------------------
 
 namespace app\content\model;
 
 use think\Model;
+
 /**
- * Description of ContentData
+ * ContentData模型，与content是关联模型
  *
  * @author wb
  */
@@ -20,39 +23,44 @@ class ContentData extends Model {
     static $_instance = NULL;
     
     //当前模型id
-    public $modelname = "";
+    public $modelid = "";
     
-    /**
-     * 取得内容模型实例
-     * @param type $modelname 模型名称
-     * @return obj
-     */
-    static public function getInstance($modelname) {
-        if (is_null(self::$_instance[$modelname]) || !isset(self::$_instance[$modelname])) {
-            self::$_instance[$modelname] = new ContentData(["modelname"=>$modelname]);
+    protected  $parent = NULL;
+
+    public function __construct($name="",$modelid=0) {
+        $data = [];
+        if(is_array($name)|| is_object($name)){
+            $this->data = $name;
+            $modelid = $this->data['modelid'];
+        }else{
+            $this->name = $name."_data";
         }
-        return self::$_instance[$modelname];
-    }
-    /**
-     * 创建内容数据模型
-     * @param type $data
-     */
-    public function __construct($data = array()) {
-        if(isset($data['modelname'])&&!empty($data['modelname'])){
-            $this->name = $data['modelname'];
-            //设置模型名称
-            $this->modelname = $data['modelname'];
+        if(!empty($modelid)){
+            $this->modelid = $modelid;
+            $modelCache = sys_cache("Model");
+            if (empty($modelCache[$this->modelid])) {
+                return false;
+            }
+            if(! $this->parent instanceof ContentData){
+                $this->parent = \app\content\model\Content::getInstance($modelid);
+            }
+            //根据模型id重新赋值name
+            $this->name = $modelCache[$this->modelid]['tablename']."_data";
+            
+            $this->connection = config("database");
+            $this->table = config("database.prefix").$this->name;
+            $this->class = get_class($this);
+            $this->db()->name($this->name);
+            $this->pk = $this->getPk($this->name);
         }
-        if(isset($data['foreignKey'])&&!empty($data['foreignKey'])){
-            $this->foreignKey = $data['foreignKey'];
-        }
-        
-        if(isset($data['foreignKey'])&&!empty($data['foreignKey'])){
-            $this->foreignKey = $data['foreignKey'];
-        }
-        
-        parent::__construct($data);
-        
     }
     
+    public function getRelation($id=0){
+        $result = [];
+        if($id>0){
+            // 执行关联定义方法
+            $result = $this->where("id", $id)->find()->toArray();
+        }
+        return $result;
+    }
 }
