c87d267e84fca1582b5c83578915828cbc2385e1
wb1491 <wb1491@126.com>
2016-10-05 15:31:59 +0800

    修改用户相关操作按钮

--- template/admin/default/admin/Index/index.php
+++ template/admin/default/admin/Index/index.php
@@ -200,51 +200,13 @@
     <div class="page-footer">
       <div class="row">
         <div class="col-xs-12 col-sm-6">
-          <span class="txt-color-white">SmartAdmin 1.5 <span class="hidden-xs"> - Web Application Framework</span> © 2014-2015</span>
+          <span class="txt-color-white">{$config.version.appname} {$config.version.version} <span class="hidden-xs"> - {$config.version.description}</span> © {$config.version.authtime}</span>
         </div>
 
         <div class="col-xs-6 col-sm-6 text-right hidden-xs">
           <div class="txt-color-white inline-block">
-            <i class="txt-color-blueLight hidden-mobile">Last account activity <i class="fa fa-clock-o"></i> <strong>52 mins ago &nbsp;</strong> </i>
-            <div class="btn-group dropup">
-              <button class="btn btn-xs dropdown-toggle bg-color-blue txt-color-white" data-toggle="dropdown">
-                <i class="fa fa-link"></i> <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu pull-right text-left">
-                <li>
-                  <div class="padding-5">
-                    <p class="txt-color-darken font-sm no-margin">Download Progress</p>
-                    <div class="progress progress-micro no-margin">
-                      <div class="progress-bar progress-bar-success" style="width: 50%;"></div>
-                    </div>
-                  </div>
-                </li>
-                <li class="divider"></li>
-                <li>
-                  <div class="padding-5">
-                    <p class="txt-color-darken font-sm no-margin">Server Load</p>
-                    <div class="progress progress-micro no-margin">
-                      <div class="progress-bar progress-bar-success" style="width: 20%;"></div>
-                    </div>
-                  </div>
-                </li>
-                <li class="divider"></li>
-                <li >
-                  <div class="padding-5">
-                    <p class="txt-color-darken font-sm no-margin">Memory Load <span class="text-danger">*critical*</span></p>
-                    <div class="progress progress-micro no-margin">
-                      <div class="progress-bar progress-bar-danger" style="width: 70%;"></div>
-                    </div>
-                  </div>
-                </li>
-                <li class="divider"></li>
-                <li>
-                  <div class="padding-5">
-                    <button class="btn btn-block btn-default">refresh</button>
-                  </div>
-                </li>
-              </ul>
-            </div>
+            <i class="txt-color-blueLight hidden-mobile">最后登录时间 <i class="fa fa-clock-o"></i> <strong>{$userInfo.last_login_time|date="Y/m/d H:i:s",###} &nbsp;</strong> </i>
+            
             <!-- end btn-group-->
           </div>
           <!-- end div-->
@@ -260,22 +222,13 @@
     <div id="shortcut">
       <ul>
         <li>
-          <a href="#ajax/inbox.html" class="jarvismetro-tile big-cubes bg-color-blue"> <span class="iconbox"> <i class="fa fa-envelope fa-4x"></i> <span>Mail <span class="label pull-right bg-color-darken">14</span></span> </span> </a>
+          <a href="javascript:void(0);" data-action="loadSUrl" data-url="{:url('admin/Adminmanage/myinfo')}" class="jarvismetro-tile big-cubes bg-color-blue"> <span class="iconbox"> <i class="fa fa-envelope fa-4x"></i> <span>管理员信息</span> </span> </a>
         </li>
         <li>
-          <a href="#ajax/calendar.html" class="jarvismetro-tile big-cubes bg-color-orangeDark"> <span class="iconbox"> <i class="fa fa-calendar fa-4x"></i> <span>Calendar</span> </span> </a>
+          <a href="javascript:void(0);" data-action="loadSUrl" data-url="{:url('admin/Adminmanage/chanpass')}" class="jarvismetro-tile big-cubes bg-color-orangeDark"> <span class="iconbox"> <i class="fa fa-calendar fa-4x"></i> <span>修改密码</span> </span> </a>
         </li>
         <li>
-          <a href="#ajax/gmap-xml.html" class="jarvismetro-tile big-cubes bg-color-purple"> <span class="iconbox"> <i class="fa fa-map-marker fa-4x"></i> <span>Maps</span> </span> </a>
-        </li>
-        <li>
-          <a href="#ajax/invoice.html" class="jarvismetro-tile big-cubes bg-color-blueDark"> <span class="iconbox"> <i class="fa fa-book fa-4x"></i> <span>Invoice <span class="label pull-right bg-color-darken">99</span></span> </span> </a>
-        </li>
-        <li>
-          <a href="#ajax/gallery.html" class="jarvismetro-tile big-cubes bg-color-greenLight"> <span class="iconbox"> <i class="fa fa-picture-o fa-4x"></i> <span>Gallery </span> </span> </a>
-        </li>
-        <li>
-          <a href="#ajax/profile.html" class="jarvismetro-tile big-cubes selected bg-color-pinkDark"> <span class="iconbox"> <i class="fa fa-user fa-4x"></i> <span>My Profile </span> </span> </a>
+          <a href="javascript:void(0);" data-action="loadSUrl" data-url="{:url('admin/Adminmanage/chanavatar')}" class="jarvismetro-tile big-cubes bg-color-purple"> <span class="iconbox"> <i class="fa fa-map-marker fa-4x"></i> <span>修改头像</span> </span> </a>
         </li>
       </ul>
     </div>
